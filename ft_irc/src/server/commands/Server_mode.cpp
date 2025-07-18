1d883c7e5e9a0a13b7e057e3de8b619dff739b3c
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-07-18 19:04:28 -0300

    oK

--- ft_irc/src/server/commands/Server_mode.cpp
+++
@@ -1,9 +0,0 @@
-#include "../includes/Server.hpp"
-#include "../includes/messages.hpp"
-
-void	Server::mode(s_commands &com)
-{
-	// this->sendBuffer[com.index].clear();
-	// this->sendBuffer[com.index] += msg_notice("Mode was called");
-	std::cout << "Chegamos no mode e esse eh o send buffer: " << this->sendBuffer[com.index] << std::endl;
-}
