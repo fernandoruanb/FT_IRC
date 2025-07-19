ce8c2a530af07cbbe9fc04cb6b4ce77a7861b58d
felipe nasser <felipecmnasser@gmail.com>
2025-07-19 19:02:46 -0300

    :pencil: MOTD

--- ft_irc/src/server/commands/motd.cpp
+++ ft_irc/src/server/commands/motd.cpp
@@ -3,5 +3,8 @@
 void	Server::motd(s_commands& com)
 {
 	this->sendBuffer[com.index].clear();
-	this->sendBuffer[com.index] = "You called MOTD\n";
+	this->sendBuffer[com.index] = SERVER_NAME " 375 " + com.client->getNickName() + " :- Message of the day -\r\n";
+	this->sendBuffer[com.index] += SERVER_NAME " 372 " + com.client->getNickName() + " :- Welcome to our IRC server!\r\n";
+	this->sendBuffer[com.index] += SERVER_NAME " 372 " + com.client->getNickName() + " :- Respect the rules.\r\n";
+	this->sendBuffer[com.index] += SERVER_NAME " 376 " + com.client->getNickName() + " :End of /MOTD command.\r\n";
 }
