2d3c2bb7eca4d6b88e628deb7c6028644d4565c2
Felipe Caldas Marques Nasser <103406806+afsser@users.noreply.github.com>
2025-07-23 17:38:06 -0300

    :pencil: implemented kick()

--- ft_irc/src/server/commands/kick.cpp
+++ ft_irc/src/server/commands/kick.cpp
@@ -7,12 +7,6 @@ void	Server::kick(s_commands& com)
 		this->sendBuffer[com.index] += msg_err_needmoreparams("KICK");
 		return ;
 	}
-	int channelIndex = getChannelsIndex(com.args[0]);
-	if (channelIndex == -1) {
-		this->sendBuffer[com.index].clear();
-		this->sendBuffer[com.index] += msg_err_nosuchchannel(com.args[0]);
-		return ;
-	}	
 	Channel* channel = this->channels->find(channelIndex)->second;
 	int clientIndex = getClientsIndex(com.fd);
 	if (!channel.isMemberOfChannel(clientIndex)) {
@@ -26,14 +20,5 @@ void	Server::kick(s_commands& com)
 		this->sendBuffer[com.index] += msg_err_usernotinchannel(com.args[1]);
 		return ;
 	}
-	if (!channel.isOperatorOfChannel(clientIndex)) {
-		this->sendBuffer[com.index].clear();
-		this->sendBuffer[com.index] += msg_err_chanoprivsneeded(com.args[0]);
-		return ;
-	}
-	std::string message = com.args.size() > 2 ? com.args[2] : "";
-	this->sendBuffer[targetIndex].clear();
-
-	// this->sendBuffer[com.index].clear();
-	// this->sendBuffer[com.index] = "You called KICK\n";
+	this->kickFromChannel(com.args[0], clientIndex, targetIndex);
 }
