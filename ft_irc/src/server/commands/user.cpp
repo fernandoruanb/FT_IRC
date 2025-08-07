19b7053cce30048ff3a8ae832ec4f738f2a4085a
Jonas Alberto Pereira <jopereir@.42sp.org.br>
2025-08-07 19:12:30 -0300

    user-update

--- ft_irc/src/server/commands/user.cpp
+++ ft_irc/src/server/commands/user.cpp
@@ -8,7 +8,7 @@ void	Server::user(s_commands	&com)
 	}
 	size_t	len = com.args.size();
 
-	if (len < 3)
+	if (len < 4)
 		return (callCmdMsg("Not enough parameters", 461, com, com.sendBuffer));
 
 	if (com.args[0] == "*" || com.args[0] == "system")
@@ -17,17 +17,20 @@ void	Server::user(s_commands	&com)
 	com.client->setUserName(com.args[0]);
 	com.client->setHost(com.args[1]);
 	com.client->setServerName(com.args[2]);
-	if (len > 3 && com.args[3][0] == ':')
+
+	if (com.args[3][0] != ':')
 	{
-		com.args[3] = com.args[3].substr(1);
-		std::string	name;
-	
-		for (size_t i = 3; i < len; i++)
-			name += " " + com.args[i];
-		
-		com.client->setRealName(name);
-		std::cout << "real name: " << com.client->getRealName() << std::endl;
+		com.sendBuffer = msg_err_needmoreparams(com.client->getNickName(), com.command);
+		return;
 	}
+
+	com.args[3] = com.args[3].substr(1);
+	std::string	name;
+
+	for (size_t i = 3; i < len; i++)
+		name += " " + com.args[i];
+	
+	com.client->setRealName(name);
 	if (com.client->getNickName() != "*")
 		com.client->setRegistered(true);
 }
